82d741e452e9e109d7a0f4491c7f19dcaf76560f
DDamien <da.descriaux@gmail.com>
2022-10-12 10:52:31 +0200

    prenier comit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ipsa eius magnam veniam molestias dolore adipisci repudiandae omnis. Laborum, rem hic expedita, reprehenderit repudiandae placeat cupiditate quisquam ex, eveniet officiis esse vero nihil maiores dolorum blanditiis asperiores beatae totam! Sit, aspernatur in dolorem, culpa assumenda dignissimos corrupti voluptate expedita itaque magni possimus quia earum, ipsum quibusdam sapiente. Ipsam voluptatem distinctio corporis consequuntur nihil adipisci. Perspiciatis consequatur enim natus id. Perferendis quidem autem, culpa hic expedita exercitationem consectetur magnam esse minus rerum suscipit, vero minima harum corrupti! Laboriosam consequatur sint odio ex animi eaque doloremque ducimus tempore soluta. Nesciunt, earum suscipit..</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptate amet fuga earum nobis aspernatur, rem impedit temporibus autem? Facilis, architecto ipsa. Saepe nulla quibusdam aspernatur enim ullam. Repellendus quam alias consectetur voluptates aut soluta earum asperiores odit nesciunt doloremque blanditiis nemo illo reprehenderit amet, officiis eos quis vitae sed magnam? Incidunt vitae distinctio temporibus libero sed blanditiis quas mollitia quae officia numquam vel quod aliquid assumenda magnam, ipsa nulla quaerat necessitatibus rem dolor atque corrupti inventore, dicta at? Cum animi, sapiente est in, laboriosam totam aliquid odit velit unde deserunt repellendus quasi quae ullam atque beatae suscipit praesentium tenetur perspiciatis? Minima, ipsam! Illum aspernatur blanditiis, libero recusandae architecto iusto mollitia cum voluptatem officiis ullam perferendis expedita non odit id doloribus quis! Id cumque nostrum laudantium aliquid odio, officiis tempore optio voluptatum consectetur iusto, adipisci repellat ducimus dolor culpa debitis enim eius sint perferendis perspiciatis placeat nulla inventore quisquam magni eligendi. Veritatis consectetur vero iusto commodi, sed, veniam voluptates eligendi assumenda iste magnam nostrum perspiciatis non obcaecati aspernatur. Sequi ut dignissimos iure impedit natus ullam explicabo voluptatum amet. Quibusdam libero aliquam, explicabo deserunt nihil voluptates molestias repellat dignissimos natus qui? Tempore placeat cumque, ducimus temporibus officiis rem nam voluptates ullam!</p>
+        </div>
+    );
+};
+
+export default About;
